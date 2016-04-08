38f967cbe3912564ac163c441d2f130996d0ba40
Boyzie Mathis <boyzielee@gmail.com>
2016-04-08 15:49:08 -0400

    making some progress on question #2....

--- js/main.js
+++ js/main.js
@@ -1,28 +1,37 @@
-// Show me how to calculate the average price of all items.
+// 1. SHOW ME HOW TO CALCULATE the average price of all items.
 
-var allPrices = items.map(function (item) {
+// To get a sum of all prices, we first need all prices
+// This `map` will give us an array of all prices
+var allPrices = items.map( function (item) {
   return item.price;
-
 });
 
-function addUpItemPrices (previous, current){
-  return previous + current;
-}
+// Now that I have all the prices, we can `reduce` them down to
+// a single value, which is the sum of all prices
+var priceSum = allPrices.reduce(function (prev, curr) {
+  return prev + curr;
+});
 
-var priceSum = allPrices.reduce (addUpItemPrices);
-
-console.log(priceSum);
-// we want to reduce the object prices, not the object. This is why we MAP. Map builds a new array based on what I tell it to return.
-
+// Once we have the sum, we can divide by the number (length) of
+// items we have
 var avg = priceSum / items.length;
 
+// Let's now round that to 2 decimals
 var final = avg.toFixed(2);
 
+// Finally, lets build our answer string
 var answer1 = "The average price is $" + final;
 
-console.log(final);
-
-// Find my answere area in the DOM
+// Find my "answer" area in the DOM
 var answer1Area = document.querySelector('#answer1');
-// Put our answer on the page.
-answer1Area.innerHTML = answer1
+
+// Put our answer on the page
+answer1Area.innerHTML = answer1;
+
+
+
+// 2. SHOW ME HOW TO GET AN ARRAY of items that cost between $14.00 and $18.00 USD
+
+var itemsBetween = ['costLow', 'costHigh'];
+costLow = 14;
+costHigh =18;
